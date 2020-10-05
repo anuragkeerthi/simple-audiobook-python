dc86a7f0c3cd52996d5a1d326eaa0fe9f406eb2a
Anurag <anurag@pop-os.localdomain>
2020-10-05 23:45:44 +0530

    Final Commit

--- audiobook.py
+++ audiobook.py
@@ -1,5 +1,14 @@
 import pyttsx3
+import PyPDF2
+
+book = open('.pdf', 'rb') # Insert pdf file name here
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
 
 speaker = pyttsx3.init()
-speaker.say('Hi, I am talking. Is it audible')
+# speaker.say('Hi, I am talking. Is it audible')
+page = pdfReader.getPage(7) # Starting Page number
+text = page.extractText()
+speaker.say(text)
 speaker.runAndWait()
