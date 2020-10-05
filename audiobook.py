f756074d176f2d3664b8bad04b63e826fb384f1e
Anurag <anurag@pop-os.localdomain>
2020-10-05 23:53:56 +0530

    Final Commit

--- audiobook.py
+++ audiobook.py
@@ -8,7 +8,7 @@ print(pages)
 
 speaker = pyttsx3.init()
 # speaker.say('Hi, I am talking. Is it audible')
-for page_number in range(pages):
+for page_number in range(0, pages):
     page = pdfReader.getPage(page_number) 
     text = page.extractText()
     speaker.say(text)
